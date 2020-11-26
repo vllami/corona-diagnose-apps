d118f512778841c7afd0ff50c1bea16c11183fbc
nurzainpradana <pradananurzain@gmail.com>
2020-11-26 09:12:08 +0700

    create Const.java for key constant

---
+++ app/src/main/java/id/uinic/diagnosacovid/api/Api.java
@@ -0,0 +1,24 @@
+package id.uinic.diagnosacovid.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Api {
+    private static Retrofit retrofit = null;
+    public static Retrofit getApi() {
+        if (retrofit == null) {
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit;
+    }
+}
