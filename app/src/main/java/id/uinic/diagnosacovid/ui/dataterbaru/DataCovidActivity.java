e726dc0500668a4a0190ce539e0bdee7c4672671
nurzainpradana <pradananurzain@gmail.com>
2020-11-28 18:03:07 +0700

    implement data binding

--- app/src/main/java/id/uinic/diagnosacovid/ui/dataterbaru/DataCovidActivity.java
+++ app/src/main/java/id/uinic/diagnosacovid/ui/dataterbaru/DataCovidActivity.java
@@ -1,16 +1,20 @@
 package id.uinic.diagnosacovid.ui.dataterbaru;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import id.uinic.diagnosacovid.R;
+import id.uinic.diagnosacovid.databinding.ActivityDataCovidBinding;
 
 public class DataCovidActivity extends AppCompatActivity {
+    ActivityDataCovidBinding bind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_covid);
+        bind = DataBindingUtil.setContentView(this, R.layout.activity_data_covid);
     }
 }
